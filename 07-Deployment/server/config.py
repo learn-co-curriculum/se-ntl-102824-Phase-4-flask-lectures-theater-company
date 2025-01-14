4e6ec5fcef7b7446906aa380de8839d0b08335e7
betalantz <betalantz@gmail.com>
2025-01-14 09:44:59 -0800

    adds naming convention for constraints

--- 07-Deployment/server/config.py
+++ 07-Deployment/server/config.py
@@ -8,8 +8,22 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Metadata
+
+
 
 load_dotenv()
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata, engine_options={"echo": True})
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
@@ -19,7 +33,6 @@ app.json.compact = False
 # generate a secrete key `python -c 'import os; print(os.urandom(16))'`
 app.secret_key = os.environ.get("SECRET_KEY")
 
-db = SQLAlchemy()
 migrate = Migrate(app, db)
 
 bcrypt = Bcrypt(app)
